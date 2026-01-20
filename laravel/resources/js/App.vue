81865c043e0f280d72d75ba325142b5db156c1b0
Tamai Honoka <honokatamai8739@gmail.com>
2026-01-20 21:52:45 +0900

    feat: メモ作成画面の実装

--- laravel/resources/js/App.vue
+++ laravel/resources/js/App.vue
@@ -1,8 +1,10 @@
 <template>
     <Header />
     <router-view />
+    <TextareaForm />
 </template>
 
 <script setup>
 import Header from './components/Header.vue'
+import TextareaForm from './components/TextareaForm.vue'
 </script>
