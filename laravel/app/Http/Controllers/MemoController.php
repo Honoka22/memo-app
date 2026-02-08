842610a00a69246c8d71750aa7047e8e16b8fc1b
Tamai Honoka <honokatamai8739@gmail.com>
2026-02-09 00:39:57 +0900

    メモ取得APIの実装

--- laravel/app/Http/Controllers/MemoController.php
+++ laravel/app/Http/Controllers/MemoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Memo;
 use Illuminate\Http\Request;
 
 class MemoController extends Controller
@@ -17,6 +18,15 @@ class MemoController extends Controller
         ]);
 
         return response()->json($memo, 201);
+    }
+
+    public function index()
+    {
+        // メモを全て取得
+        $memos = Memo::orderBy('created_at', 'desc')->get();
+
+        // JSON形式でレスポンスを返す
+        return response()->json($memos);
     }
 
 }
